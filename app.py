3619a7b0a0103efc926bb12dc9b8f920845f9926
terrylashley <terry.l.lashley.pro@gmail.com>
2020-04-09 16:13:49 -0400

    update

---
+++ app.py
@@ -0,0 +1,28 @@
+# 1. import Flask
+import pandas as pd
+from sqlalchemy import create_engine
+from flask import Flask, render_template
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+rds_connection_string = "postgres:postgres@localhost:5432/employee_db"
+engine = create_engine(f'postgresql://{rds_connection_string}')
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    result = '# query postGres'   {{db.customers}}
+    print("Server received request for 'Home' page...")
+    return render_template("index.html", db = result)
+
+
+# 4. Define what to do when a user hits the /about route
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
