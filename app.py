8829d2ec6c2bb74a49caf7425fa4ecdf9d3824c4
terrylashley <terry.l.lashley.pro@gmail.com>
2020-04-14 13:47:05 -0400

    update

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ rds_connection_string = "postgres:postgres@localhost:5433/covid19_db"
 engine = create_engine(f'postgresql://{rds_connection_string}')
 
 # 3. Define what to do when a user hits the index route
-@app.route("/")
+@app.route("/")   
 def home():
-    result = pd.read_sql_query('select * from covid19', con=engine).head()  
+    result = pd.read_sql_query('select * from covid19', con=engine) 
     print("Server received request for 'Home' page...")
     return render_template("index.html", db = result)
 
